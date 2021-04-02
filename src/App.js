64407eea5002f8bd1c741cbc4bcb6a7cb68ec3ab
krazychase <krazychase@gmail.com>
2021-04-02 14:28:39 -0700

    update base game

--- src/App.js
+++ src/App.js
@@ -1,23 +1,3 @@
-import './App.css';
+import Page from './components/page'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export default Page
